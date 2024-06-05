94e6bf36cb0c4ca38b75945413af9856188cd9e0
Camilla Avila <avilacamilla@outlook.com>
2024-06-04 21:44:13 -0300

    feature: Finaliza marcação do componente Header adicionando as rotas, e adiciona tag styles para formatar displays

---
+++ src/lib/Header.svelte
@@ -0,0 +1,38 @@
+<script>
+
+</script>
+
+<nav class="header-container">
+    <ul class="header-list-item-container">
+        <li class="list-item">
+            <a href="/" class="list-item-ancor">Início</a>
+        </li>
+        <li class="list-item">
+            <a href="/about" class="list-item-ancor">Quem é a Coffee?</a>
+        </li>
+        <li class="list-item">
+            <a href="/contact" class="list-item-ancor">Contato</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    .header-container {
+        display: flex;
+        width: 100%;
+    }
+
+    .header-list-item-container {
+        display: flex;
+        gap: 2rem;
+        width: 100%;
+    }
+
+    .list-item {
+        list-style-type: none;        
+    }
+
+    .list-item-ancor {
+        text-decoration: none;
+    }
+</style>
