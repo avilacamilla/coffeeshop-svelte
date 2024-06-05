f686539c0a5e54769a817fca8110ab5e9601edcc
Camilla Avila <avilacamilla@outlook.com>
2024-06-04 21:37:24 -0300

    feature: Desenvolve base/layout da das páginas

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
-
+  import Header from "$lib/Header.svelte";
 </script>
 
 <div class="layout-container">
+    <header class="layout-header-container">
+        <Header />
+    </header>
     <!-- espaço para o componente header que será fixo -->
+
+    <main class="layout-main-container">
+        <slot />
+    </main>
 </div>
