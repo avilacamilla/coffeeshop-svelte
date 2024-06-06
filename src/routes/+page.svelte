71dadb743fda957fbe6c2521a52a357efe98307d
Camilla Avila <avilacamilla@outlook.com>
2024-06-06 20:50:06 -0300

    style:: página Home:: zera margin e padding e adiciona background color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,11 @@
 <div class="home-container">
     <h1 class="home-container-title">Página raiz</h1>
 </div>
+
+<style>
+    .home-container {
+        margin: 0;
+        padding: 0;
+        background-color: #3b2525;
+    }
+</style>
